0ebdd7dcd09491db01f57561f913c3323a334dd2
mohamad Javed <maj38@njit.edu>
2018-10-14 15:42:45 -0400

    Task: Added more students

--- public/index.php
+++ public/index.php
@@ -21,16 +21,41 @@ class main  {
 class html {
 
     public static function generateTable($records) {
-
         $count = 0;
         $table = "";
-
         foreach ($records as $record) {
-            $table .= "<html><head></head><body><table class='table table-striped'>";
+            $table .= "<html>
 
+
+                   <thead>
+                      <title>Bootstrap Example</title>
+                       <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
+                       <meta charset=\"utf-8\">
+                       <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+                       <link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\"/>
+
+                   </thead>
+           <tbody>
+                       <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">
+           <div class=\"container\">
+           <link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\"/>
+
+
+
+
+           <style>
+           table{ table-layout: fixed;}
+           table th, table td {overflow: hidden;}
+
+           <style/>
+
+
+           }
+
+           </style>
+                   <table class='table table-striped table-bordered'>";
 
             if ($count == 0) {
-
                 $array = $record->returnArray();
                 $fields = array_keys($array);
                 $table .= "<tr>";
@@ -38,9 +63,9 @@ class html {
                 foreach ($fields as $field) {
                     $table .= "<th>" . $field . "</th>";
                     $count = 1;
-
                 }
                 $table .= "</tr>";
+
             }
             $array = $record->returnArray();
             $values = array_values($array);
@@ -50,28 +75,22 @@ class html {
             }
             $table .= "</tr>";
         }
+        $table .= "</table>
 
-            $table .= "</table></body></html>";
+<link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\"/>
+       </div>
+           <link href=\"style.css\" rel=\"stylesheet\" type=\"text/css\"/>
+       </tbody></html>";
         return $table;
-        }
     }
-
-
+}
 class csv {
-
-
     static public function getRecords($filename) {
-
         $file = fopen($filename,"r");
-
         $fieldNames = array();
-
         $count = 0;
-
-
         while(! feof($file))
         {
-
             $record = fgetcsv($file);
             if($count == 0) {
                 $fieldNames = $record;
@@ -80,45 +99,29 @@ class csv {
             }
             $count++;
         }
-
         fclose($file);
         return $records;
     }
-
 }
-
 class record {
-
     public function __construct(Array $fieldNames = null, $values = null )
     {
         $record = array_combine($fieldNames, $values);
-
         foreach ($record as $property => $value) {
             $this->createProperty($property, $value);
         }
-
     }
-
     public function returnArray() {
         $array = (array) $this;
-
         return $array;
     }
-
     public function createProperty($name = 'first', $value = 'keith') {
-
         $this->{$name} = $value;
-
     }
 }
-
 class recordFactory {
-
     public static function create(Array $fieldNames = null, Array $values = null) {
         $record = new record($fieldNames, $values);
-
         return $record;
-
     }
 }
-
